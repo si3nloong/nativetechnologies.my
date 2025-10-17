fdb9b6859a353a13dc1b980f86d88a304129c65b
si3nloong <sianloong90@gmail.com>
2025-10-17 18:13:55 +0800

    fix: `out` path

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			out: 'public'
+		})
+	},
 	extensions: ['.svelte', '.svx']
 };
 
